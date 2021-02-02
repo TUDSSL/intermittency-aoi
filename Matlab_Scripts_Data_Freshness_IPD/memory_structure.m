92b5c9aa136a34eee1e7380e409b52cb6d36c841
Przemysław Pawełczak <p.pawelczak@tudelft.nl>
2021-02-02 22:36:28 +0100

    Update memory_structure.m

--- Matlab_Scripts_Data_Freshness_IPD/memory_structure.m
+++ Matlab_Scripts_Data_Freshness_IPD/memory_structure.m
@@ -22,9 +22,11 @@
 %SOFTWARE.
 
 %This script generates Figure 3b (E[f] against Aoi for various memory structures)
-%Taken from the paper "Data Freshness in Mixed-Memory
-%Intermittently-Powered Systems" by James Scott Broadhead and Przemysław Pawełczak. 
+%in the paper "Data Freshness in Mixed-Memory Intermittently-Powered Systems" 
+%by James Scott Broadhead and Przemysław Pawełczak.
+
 %To cite this publication please use the following BiBTeX entry.
+
 %@misc{broadhead:intermittent-aoi:2021,
 %  title = {Data Freshness in Mixed-Memory Intermittently-Powered Systems},
 %  author = {James Scott {Broadhead} and Przemys\l{}aw {Pawe\l{}czak}},
@@ -32,6 +34,7 @@
 %  month = feb,
 %  note = {submitted for publication}
 %}
+
 
 P = 500;                %Total processing time
 I = 200;                 %System idle time
