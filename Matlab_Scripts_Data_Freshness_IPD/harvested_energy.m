119e45ccce20a2af1ea2e43fa225ef08cba2dc02
Przemysław Pawełczak <p.pawelczak@tudelft.nl>
2021-02-02 22:36:58 +0100

    Update harvested_energy.m

--- Matlab_Scripts_Data_Freshness_IPD/harvested_energy.m
+++ Matlab_Scripts_Data_Freshness_IPD/harvested_energy.m
@@ -22,9 +22,11 @@
 %SOFTWARE.
 
 %This script generates Figure 3a (E[h] against Aoi for RF1 and RF2) 
-%Taken from the paper "Data Freshness in Mixed-Memory
-%Intermittently-Powered Systems" by James Scott Broadhead and Przemysław Pawełczak. 
+%in the paper "Data Freshness in Mixed-Memory Intermittently-Powered Systems" 
+%by James Scott Broadhead and Przemysław Pawełczak.
+
 %To cite this publication please use the following BiBTeX entry.
+
 %@misc{broadhead:intermittent-aoi:2021,
 %  title = {Data Freshness in Mixed-Memory Intermittently-Powered Systems},
 %  author = {James Scott {Broadhead} and Przemys\l{}aw {Pawe\l{}czak}},
